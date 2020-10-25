fb65955c1aa1b00ab577c93a380f44e9354f16de
Akira Komamura <akira.komamura@gmail.com>
2020-10-25 18:20:13 +0900

    Add version

--- default.nix
+++ default.nix
@@ -4,6 +4,7 @@ let
   gitignore = import sources."gitignore.nix" {
     inherit (pkgs) lib;
   };
+  version = "0.1";
 in
 pkgs.runCommandNoCC "nix-desktop"
 {
@@ -12,9 +13,12 @@ pkgs.runCommandNoCC "nix-desktop"
   share=$out/share
   mkdir -p $share
   cp -r -t $share $src/lib $src/main.nix
+  substituteInPlace $share/main.nix \
+    --replace VERSION "${version}"
 
   mkdir -p $out/bin
   cp $src/bin/nix-desktop $out/bin
   substituteInPlace $out/bin/nix-desktop \
-    --replace "'main.nix'" "$share/main.nix"
+    --replace "'main.nix'" "$share/main.nix" \
+    --replace "VERSION" "${version}"
 ''
